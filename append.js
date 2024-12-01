4dd0daacbb716d174f292b0b1582510078802eff
Niharika-vanam <vanamniha@gmail.com>
2024-12-01 20:57:29 +0530

    added console

--- append.js
+++ append.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
-fs.appendFile("input.js", " \ Be Patient", function(err){
-    if(err)
-    console.log("error");
-})
-    console.log("success");
+fs.appendFile("input.js", "  Be Patient", function (err) {
+  if (err) console.log("error");
+});
+console.log("success");
+console.log("Hello");
